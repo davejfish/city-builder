9d699a5bcd5039dbe5984f6d25c37ba605f72d4a
Dave Fisher <dave.j.fish@gmail.com>
2022-06-08 19:16:21 -0700

    city designer controls update city state, updateDisplay can update all display elements by city state

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 // import needed modules
 
 // state
-
+const city = {
+    name: 'yamagata',
+    climate: 'cold',
+    arch: 'hotspring',
+    slogan: 'the snowy shogi mountain city',
+};
 // components
     // component
     // define and grab DOM elements
@@ -13,6 +18,44 @@
         // re-display components (which ones?)
     // optional: handle functions for shared event handler logic
 
+// input dom elements
+const inputSection = document.getElementById('input-section');
+const nameInput = inputSection.querySelector('input');
+const [climateInput, archInput] = inputSection.querySelectorAll('select');
+const sloganInput = inputSection.querySelector('textarea');
+
+// display dom elements
+const nameDisplay = document.getElementById('name-display');
+const climateDisplay = document.getElementById('climate-display');
+const archDisplay = document.getElementById('arch-display');
+const sloganDisplay = document.getElementById('slogan-display');
+
+// event handlers
+nameInput.addEventListener('input', () => {
+    city.name = nameInput.value;
+    nameDisplay.textContent = nameInput.value;
+});
+
+climateInput.addEventListener('change', () => {
+    city.climate = climateInput.value;
+    climateDisplay.src = 'assets/' + climateInput.value + '.png';
+});
+
+archInput.addEventListener('change', () => {
+    city.arch = archInput.value;
+    archDisplay.src = 'assets/' + archInput.value + '.png';
+});
+
+sloganInput.addEventListener('input', () => {
+    city.slogan = sloganInput.value;
+    sloganDisplay.textContent = sloganInput.value;
+});
+
+function updateDisplay() {
+    nameDisplay.textContent = city.name;
+    climateDisplay.src = 'assets/' + city.climate + '.png';
+    archDisplay.src = 'assets/' + city.arch + '.png';
+    sloganDisplay.textContent = city.slogan;
+}
 // page load actions
-
-
+updateDisplay();
